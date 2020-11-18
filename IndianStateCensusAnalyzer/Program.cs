4a6204f614504cefb87c626007a76d4d6e408efe
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-18 18:14:31 +0530

    Add project files.

---
+++ IndianStateCensusAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyzer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to indian state analyzer");
+        }
+    }
+}
